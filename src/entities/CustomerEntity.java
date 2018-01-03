ab630263c0b5fb4821b3eb8f283647450ba4cfb2
Talgross <Talgross@10.10.4.253>
2018-01-03 14:31:18 +0200

    s

--- src/entities/CustomerEntity.java
+++ src/entities/CustomerEntity.java
@@ -2,11 +2,10 @@ package entities;
 
 public class CustomerEntity extends UserEntity {
 
-//	private long creditCardNumber;
-//	private String address;
-//	private long customerID;
-	//private double subscriptionDiscount;
-	
-	
-	
+	private long creditCardNumber;
+	private String address;
+	private long customerID;
+	private double subscriptionDiscount;
+	private  accountStatus;
+		
 }
