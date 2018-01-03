3440779e474357582f0b37d360f85180058ce16a
Talgross <Talgross@10.10.4.253>
2018-01-03 17:39:14 +0200

    more entities

---
+++ src/entities/CardEntity.java
@@ -0,0 +1,43 @@
+package entities;
+/**
+ * This class represents the card added to the order
+ * CardEntity.java
+ *
+ * @author Eliran Toledano
+ * @author Lana Krikheli
+ * @author Katya Yakovlev
+ * @author Tal Gross
+ *
+ * Project Name gitProjectX
+ *
+ */
+public class CardEntity {
+	
+	private String text;
+	/**
+	 * 
+	 * Constructor for the CardEntity.java class
+	 * @param txt
+	 */
+	public CardEntity(String txt)
+	{
+		this.text=txt;
+	}
+
+	/**
+	 * Getter for the text
+	 * @return the text
+	 */
+	public String getText() {
+		return text;
+	}
+
+	/**
+	 * Setter for the text
+	 * @param text the text to set
+	 */
+	public void setText(String text) {
+		this.text = text;
+	}
+
+}
